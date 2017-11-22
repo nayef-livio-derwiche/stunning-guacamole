9be5442cfe3cdc6fc72210d6fd95ca5bba71b445
nayef-livio-derwiche <33095405+nayef-livio-derwiche@users.noreply.github.com>
2017-11-22 15:41:03 +0100

    preliminary tests with logistic regression
    definition and training of a logistic model with time window, train/test split and strategy evaluation on real returns.
    
    TO DO:
    - Clean and split the code
    - Comment the code
    - Fine-tune the model
    - Test new models

---
+++ logistic_reg_test.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 22:15:57 2017
+
+@author: Nayef
+"""
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+time_window = 6
+
+"""
+TO DO:
+- neural network
+- regularization
+- batching and iterations
+- proper train/test loop and iterations
+
+"""
+
+def load_data():
+    df = pd.read_excel("SP500.xlsx")
+    #symbols = list(df.keys())
+    data = df.as_matrix()
+    del(df)
+    for i in range(data.shape[0]-1):
+        data[i,:] = np.log(data[i+1,:]/data[i,:])
+    data = data[:-1,:]
+    mean = data.mean()
+    std = data.std()
+    data = (data - mean)/std
+    labels = np.zeros(data.shape)
+    labels[data>0] = 1
+    return data, labels, mean, std
+
+def prepare_model(data_shape):
+    x = tf.placeholder(tf.float32, shape=[None, time_window * data_shape[1]])
+    y_true = tf.placeholder(tf.float32, shape = [None, data_shape[1]])
+    W = tf.Variable(tf.zeros([time_window * data_shape[1], data_shape[1]]))
+    b = tf.Variable(tf.zeros([data_shape[1]]))    
+    
+    z = tf.matmul(x,W) + b
+    y = tf.sigmoid(z)
+    loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true, logits=z)) + 10 * tf.nn.l2_loss(W)
+    
+    return loss, x, y, y_true, W, b
+    
+def data_generator(data, labels, batch_size = 1):
+    i = 0
+    while True:
+        x = np.zeros([batch_size, time_window * data.shape[1]])
+        y_true = np.zeros([batch_size, data.shape[1]])
+        for k in range(batch_size):
+            x[k,:] = np.reshape(data[i:i+time_window,:], [time_window * data.shape[1]])
+            y_true[k,:] = labels[i+time_window,:]
+        yield x, y_true
+        i += 1
+        if i == data.shape[0] - time_window:
+            i = 0
+            
+def random_strategy(time, nb_stocks):
+    return normalize_strategy(np.random.rand(time, nb_stocks))
+
+def hold_all_strategy(time, nb_stocks):
+    return normalize_strategy(np.ones([time, nb_stocks]))
+
+def normalize_strategy(strategy):
+    return (strategy.transpose()/strategy.sum(1)).transpose()
+
+def evaluate_strategy(eval_data, strategy):
+    strategy = normalize_strategy(strategy)
+    R = np.zeros(eval_data.shape[0] + 1)
+    R[0] = 1
+    for i in range(eval_data.shape[0]):
+        R[i+1] = R[i] * np.dot(np.exp(eval_data[i,:]), strategy[i,:])
+    return R
+
+sess = tf.Session()
+data, labels, mean, std = load_data()
+eval_data = data[-52:] * std + mean
+train_data = data[:-52]
+train_labels = labels[:-52]
+test_data = data[-52:]
+test_labels = labels[-52:] 
+loss, X, Y, Y_true, W, b = prepare_model(data.shape)
+
+data_gen = data_generator(train_data, train_labels, batch_size=1)
+optimizer = tf.train.GradientDescentOptimizer(0.1)
+train = optimizer.minimize(loss)
+init = tf.global_variables_initializer()
+sess.run(init)
+
+for k in range(train_data.shape[0]*5):
+    x, y = next(data_gen)
+    sess.run(train, {X:x, Y_true:y})
+print(sess.run(loss, {X:x, Y_true:y}))
+
+strategy_random = random_strategy(eval_data.shape[0], eval_data.shape[1])
+strategy_hold = hold_all_strategy(eval_data.shape[0], eval_data.shape[1])
+
+strategy_logistic = np.ones(test_data.shape)
+for i in range(strategy_logistic.shape[0] - time_window):
+    historic_window = np.reshape(test_data[i:i+time_window,:], [time_window * test_data.shape[1]])
+    strategy_logistic[i+time_window] = sess.run(Y,{X:[historic_window]})
+preds = strategy_logistic
+strategy_logistic = normalize_strategy(strategy_logistic)
+
+
+strategy_logistic_train = np.ones(train_data.shape)
+for i in range(strategy_logistic_train.shape[0] - time_window):
+    historic_window = np.reshape(train_data[i:i+time_window,:], [time_window * train_data.shape[1]])
+    strategy_logistic_train[i+time_window] = sess.run(Y,{X:[historic_window]})
+preds2 = strategy_logistic_train
+strategy_logistic_train = normalize_strategy(strategy_logistic_train)
+
+R_rand = evaluate_strategy(eval_data, strategy_random)
+R_hold = evaluate_strategy(eval_data, strategy_hold)
+R_logistic = evaluate_strategy(eval_data, strategy_logistic)
+R_logistic_train = evaluate_strategy(train_data * std + mean, strategy_logistic_train)
+R_random_train = evaluate_strategy(train_data * std + mean, random_strategy(train_data.shape[0], train_data.shape[1]))
+
+print(R_logistic[-1])
+print(R_rand[-1])
+print(R_hold[-1])
+print("Cumulative excess returns:")
+plt.plot(R_logistic - R_hold)
+
+"""
+
+def generator_dem():
+    n = 0    
+    while True:
+        yield n
+        n *= 2
+"""   
